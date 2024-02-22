13137d8d90b40b07074f46fc7d8325ab3dd416ff
Aey Tato <55373822+thungong@users.noreply.github.com>
2024-02-22 12:25:04 +0700

    Update pdfconverter.py

--- pdfconverter.py
+++ pdfconverter.py
@@ -13,7 +13,7 @@ VERSION = "1.2.29"
 CONTACT_INFO = """
 **Contact Information:**
 
-- Email: thungong@thatwhy.me
+- Email: aeytato@thatwhy.me
 - Website: [https://apps.thatwhy.me](https://apps.thatwhy.me)
 """
 
